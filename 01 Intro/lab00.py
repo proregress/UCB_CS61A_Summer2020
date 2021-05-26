1f499e996163c380a1d1d2a062b4edb7b5973b4c
proregress <37496223+proregress@users.noreply.github.com>
2021-05-27 00:30:30 +0800

    Create lab00.py

---
+++ 01 Intro/lab00.py
@@ -0,0 +1,33 @@
+def twenty_twenty():
+    """Come up with the most creative expression that evaluates to 2020,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty()
+    2020
+    """
+    return (100-80)*100+20
+
+
+"""
+以下为终端运行结果
+"""
+$ python3 ok
+=====================================================================
+Assignment: Lab 0
+OK, version v1.18.1
+=====================================================================
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Running tests
+
+---------------------------------------------------------------------
+Test summary
+    3 test cases passed! No cases failed.
+
+Backup... 100% complete
+Backup past deadline by 333 days, 9 hours, 22 minutes, and 9 seconds
+Backup successful for user: ____@163.com
+
+  
+
+
